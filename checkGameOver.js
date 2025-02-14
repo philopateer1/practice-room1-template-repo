9b550635dc495f50ed45da0838cab80a1c0eec03
Retag <eslamrojy@gmail.com>
2025-02-14 10:44:16 +0200

    Retag did this :) Check and Display

---
+++ checkGameOver.js
@@ -0,0 +1,7 @@
+async function checkGameOver() {
+    console.log("Checking game over conditions...");
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    console.log("Game over conditions checked");
+}
+
+checkGameOver();
