57e16e4a6e13194a51646efb7d260521e42de208
Retag <eslamrojy@gmail.com>
2025-02-14 10:04:42 +0200

    calculate score

---
+++ calculateScore.js
@@ -0,0 +1,7 @@
+async function calculateScore() {
+    console.log("Calculating score...");
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    console.log("Score calculated");
+}
+
+calculateScore();
